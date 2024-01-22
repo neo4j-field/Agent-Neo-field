3a8360ba26ce503370f5ed53123d9abbe9c05e69
runfourestrun <90913666+runfourestrun@users.noreply.github.com>
2024-01-22 08:22:16 -0700

    google secret manager abstraction/class implemented.

--- src/main/python/airflow/extract/fetcher/secret_manager.py
+++ src/main/python/airflow/extract/fetcher/secret_manager.py
@@ -1,23 +1,28 @@
-from google.cloud import secretmanager
-from google.oauth2 import service_account
 import os
 import json
 import base64
+from typing import Optional, Union, Dict
+from google.oauth2 import service_account
+from google.cloud import secretmanager
 
 
 class SecretManager:
-    def __init__(self, service_account_info=None, project_id: str = None):
+    def __init__(self, service_account_info: Optional[Union[str, Dict]] = None, project_id: Optional[str] = None):
         self.project_id = project_id or os.getenv('GCP_PROJECT_ID')
 
         if service_account_info:
-            # If service account info is provided, create credentials from it
+            if isinstance(service_account_info, str):
+                # Decode the base64-encoded string
+                decoded_bytes = base64.b64decode(service_account_info)
+                # Load the JSON content
+                service_account_info = json.loads(decoded_bytes.decode('utf-8'))
+
             credentials = service_account.Credentials.from_service_account_info(service_account_info)
             self.client = secretmanager.SecretManagerServiceClient(credentials=credentials)
         else:
-            # Otherwise, use default credentials (which might be from the environment or default service account)
             self.client = secretmanager.SecretManagerServiceClient()
 
-    def access_secret_version(self, secret_id=None, version_id="latest"):
+    def access_secret_version(self, secret_id: str, version_id: str = "latest"):
         name = f"projects/{self.project_id}/secrets/{secret_id}/versions/{version_id}"
         response = self.client.access_secret_version(request={"name": name})
         secret_data = response.payload.data.decode("UTF-8")
--- src/main/python/airflow/extract/tests/test_secret_manager_live_connection.py
+++ src/main/python/airflow/extract/tests/test_secret_manager_live_connection.py
@@ -9,7 +9,7 @@ class TestSecretManagerReal(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        env_file_path = '/src/main/python/airflow/extract/gcpfetch/env.json'
+        env_file_path = '/Users/alexanderfournier/Downloads/Agent-Neo-field/src/main/python/airflow/extract/gcpfetch/env.json'
 
         with open(env_file_path, 'r') as f:
             config = json.load(f)
@@ -19,26 +19,25 @@ class TestSecretManagerReal(unittest.TestCase):
         cls.gcp_other_articles_bucket = config.get('GCP_OTHER_ARTICLES_BUCKET')
         cls.gcp_processed_docs = config.get('GCP_PROCESSED_DOCS')
         cls.project_id = config.get('GCP_PROJECT_ID')
+        cls.service_account_config = config.get('GOOGLE_SERVICE_ACCOUNT')
 
-        service_account_info = base64.b64decode(config.get('GOOGLE_SERVICE_ACCOUNT')).decode('utf-8')
-        cls.service_account_dict = json.loads(service_account_info)
 
         required_vars = [
             cls.gcp_sitemaps_bucket,
             cls.gcp_practitioners_guide_sites_bucket,
             cls.gcp_other_articles_bucket,
-            cls.service_account_dict,
             cls.gcp_processed_docs,
-            cls.project_id
+            cls.project_id,
+            cls.service_account_config
         ]
 
         if not all(required_vars):
             raise ValueError("One or more required environment variables are not set.")
 
     def test_access_secret_version(self):
-        secret_id = 'projects/817662103348/secrets/GCP_PROCESSED_DOCS'
+        secret_id = 'GCP_OTHER_ARTICLES_BUCKET'
 
-        sm = SecretManager(service_account_info=self.service_account_dict,project_id=self.project_id)
+        sm = SecretManager(service_account_info=self.service_account_config,project_id=self.project_id)
         secret = sm.access_secret_version(secret_id)
         print(secret)
 
