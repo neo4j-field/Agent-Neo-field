18a86c7aa4e9469063244a1e7670a36a68a7751a
runfourestrun <90913666+runfourestrun@users.noreply.github.com>
2024-01-22 07:39:03 -0700

    client access to gcp secret manager

--- src/main/python/airflow/extract/fetcher/secret_manager.py
+++ src/main/python/airflow/extract/fetcher/secret_manager.py
@@ -1,13 +1,23 @@
 from google.cloud import secretmanager
+from google.oauth2 import service_account
 import os
 import json
+import base64
+
 
 class SecretManager:
-    def __init__(self, project_id: str = None):
+    def __init__(self, service_account_info=None, project_id: str = None):
         self.project_id = project_id or os.getenv('GCP_PROJECT_ID')
-        self.client = secretmanager.SecretManagerServiceClient()
 
-    def access_secret_version(self, secret_id = None, version_id="latest"):
+        if service_account_info:
+            # If service account info is provided, create credentials from it
+            credentials = service_account.Credentials.from_service_account_info(service_account_info)
+            self.client = secretmanager.SecretManagerServiceClient(credentials=credentials)
+        else:
+            # Otherwise, use default credentials (which might be from the environment or default service account)
+            self.client = secretmanager.SecretManagerServiceClient()
+
+    def access_secret_version(self, secret_id=None, version_id="latest"):
         name = f"projects/{self.project_id}/secrets/{secret_id}/versions/{version_id}"
         response = self.client.access_secret_version(request={"name": name})
         secret_data = response.payload.data.decode("UTF-8")
