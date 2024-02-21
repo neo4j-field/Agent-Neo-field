e37dab3ef8f85b6bd332f21910d4f590633fcc01
Morgan Senechal <morgan@neo4j.com>
2024-02-21 22:43:13 +0000

    UI Refactoring + Needle StarterKit Chatbot

--- src/main/app/frontend/src/App.js
+++ src/main/app/frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, { createContext, useState } from 'react';
 import Sidebar from './components/Sidebar';
+import ChatInterfaceStarterKit from './components/ChatInterfaceStarterKit';
 import ChatInterface from './components/ChatInterface';
-import { NeedleThemeProvider, Logo } from '@neo4j-ndl/react';
-import './App.css';
+import { NeedleThemeProvider, Logo, Typography } from '@neo4j-ndl/react';
+//import './App.css';  not needed anymore
 import './tailwind.css';
 import '@neo4j-ndl/base/lib/neo4j-ds-styles.css';
 
 export const AppContext = createContext();
+const theme = 'dark'; //Ideally you would have a toggle on screen to let the user update the theme mode
 
 function App() {
     const [settings, setSettings] = useState({
@@ -17,17 +19,20 @@ function App() {
     });
 
     return (
-        <NeedleThemeProvider theme="dark">
+        <NeedleThemeProvider theme={theme} wrapperProps={{ isWrappingChildren: false }}>
             <AppContext.Provider value={{ settings, setSettings }}>
-                <div className="app flex">
+                <div className={`flex ${theme === 'dark' ? 'ndl-theme-dark' : 'ndl-theme-light'} n-bg-palette-neutral-bg-weak`}>
                     <Sidebar />
-                    <div className="flex flex-col justify-center items-center">
-                        {/* Example usage of Logo, assuming you want it at the top of your sidebar or app */}
-                        <div className="app-title flex justify-center items-center p-4">
-                            <Logo color="white" type="full" className="n-w-24" />
-                            <h1 className="text-white text-2xl font-bold ml-4">Agent-Neo</h1>
-                        </div>
-                        <ChatInterface />
+                    <div className="flex w-full flex-col justify-center items-center">
+                        <section className="flex justify-center items-center p-4">
+                            <Logo color="white" type="full" className="h-8 min-h-12 min-w-32" />
+                            <Typography variant="h2" className="ml-4">Agent-Neo</Typography>
+                        </section>
+                        {/* Uncomment for the same chat visual you had */}
+                        {/* <ChatInterface />  */}
+                        
+                        {/* Uncomment for the chatbot from Needle starter kit */}
+                        <ChatInterfaceStarterKit />
                     </div>
                 </div>
             </AppContext.Provider>
--- src/main/app/frontend/src/components/Sidebar.js
+++ src/main/app/frontend/src/components/Sidebar.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
-import { AppContext } from '../App';
-import { SegmentedControl, Switch, TextInput, Label} from '@neo4j-ndl/react';
+import { SegmentedControl, Switch, Typography} from '@neo4j-ndl/react';
 
+import { AppContext } from '../App';
 
 function Sidebar() {
   const { settings, setSettings } = useContext(AppContext);
@@ -21,55 +21,45 @@ function Sidebar() {
     }));
   };
 
-
-
-
   return (
-      <div className="w-80 flex-shrink-0 p-5 border-r overflow-y-auto">
-          <div className="mb-4 p-4">
-              <h3 className="text-lg font-semibold mb-2">Select LLM</h3>
+      <div className="w-80 flex flex-col gap-y-12 flex-shrink-0 p-5 border-r overflow-y-auto">
+          <section className="flex flex-col gap-y-4">
+              <Typography variant="h3" >LLM</Typography>
               <SegmentedControl
                   selected={settings.selectedLLM}
                   onChange={handleLLMChange}
-                  size="small"
+                  size="medium"
                   className="w-full"
               >
-                  {/* Wrapping each SegmentedControl.Item in a div with Tailwind padding for vertical spacing */}
                   {['chat-bison 2k', 'chat-bison 32k', 'GPT-4 8k', 'GPT-4 32k'].map((llm) => (
-                      <div key={llm} className="my-10"> {/* Adds vertical padding to each item */}
-                          <SegmentedControl.Item value={llm} className="py-2">
-                              {llm}
-                          </SegmentedControl.Item>
-                      </div>
-                  ))}
+                        <SegmentedControl.Item key={llm} value={llm}>
+                            <Typography variant='body-small'>{llm}</Typography>
+                        </SegmentedControl.Item>
+                    ))}
               </SegmentedControl>
-          </div>
-          <div className="mb-4">
-              <h3 className="text-lg font-semibold mb-2">Select Temperature</h3>
-              <input
-                  className="w-full h-2 rounded-lg appearance-none cursor-pointer dark:bg-gray-700 custom-slider" // Tailwind classes for initial styling + custom-slider for the thumb and track
-                  type="range"
-                  name="temperature"
-                  min="0"
-                  max="1"
-                  step="0.05"
-                  value={settings.temperature}
-                  onChange={handleChange}
-              />
-              <div className="text-center mt-1">{settings.temperature}</div>
-          </div>
-          <div className="mb-4">
-              <label className="flex items-center space-x-3">
-                  <h3 className="text-lg font-semibold">Use Grounding?</h3>
-                  <input
-                      type="checkbox"
-                      name="useGrounding"
-                      checked={settings.useGrounding}
-                      onChange={handleChange}
-                      className="form-checkbox h-5 w-5" // Tailwind classes for styling checkboxes
-                  />
-              </label>
-          </div>
+          </section>
+          <section className='flex flex-col gap-y-4'>
+            <Typography variant='h3'>Temperature</Typography>
+            <input className="w-full h-2 rounded-lg appearance-none cursor-pointer range-lg"
+                type="range" 
+                name="temperature" 
+                min="0" 
+                max="1"
+                step='0.05'
+                value={settings.temperature}
+                onChange={handleChange}
+            />
+            <Typography variant='body-large' className='text-center'>{settings.temperature}</Typography>
+          </section>
+          <section className='flex flex-col gap-y-4'>
+            <Switch
+                checked={settings.useGrounding}
+                name="useGrounding"
+                label={<Typography variant='subheading-small'>Grounding</Typography>}
+                labelBefore
+                onChange={handleChange}
+            />
+          </section>
       </div>
   );
 }
