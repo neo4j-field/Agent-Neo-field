7540b916a0cc1cc20341c06746ffbfb6d8a22c35
alex <a.gilmore1993@gmail.com>
2024-04-18 13:16:51 -0500

    rating refactor and tests

--- src/main/app/backend/objects/rating.py
+++ src/main/app/backend/objects/rating.py
@@ -1,6 +1,6 @@
 from typing import List, Dict, Union
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
 
 
 class Rating(BaseModel):
@@ -8,34 +8,19 @@ class Rating(BaseModel):
     Contains the message rating input from the frontend.
     """
 
-    session_id: str
-    conversation_id: str
-    message_id: str
-    value: str
-    message: str = ""
-
-    def __init__(self, *a, **kw) -> None:
-        super().__init__(*a, **kw)
-
-        self._assert_proper_ids()
-        self._assert_proper_rating()
+    session_id: str = Field(pattern=r'^s-.*', description="The session ID.")
+    conversation_id: str = Field(pattern=r'^conv-.*', description="The conversation ID.")
+    message_id: str = Field(pattern=r'^llm-.*', description="The LLM response message ID.")
+    value: str = Field(description="Feedback must be 'Good' or 'Bad'.")
+    message: str = Field(default="", description="The feedback message for a given LLM response.")
     
-    def _assert_proper_ids(self) -> None:
-        """
-        Validate the ID prefixes.
-        """
-
-        if not self.session_id.startswith("s-"):
-            raise ValueError("Invalid session_id prefix. Must start with s-")
-        if not self.conversation_id.startswith("conv-"):
-            raise ValueError("Invalid conversation_id prefix. Must start with conv-")
-        if not self.message_id.startswith("llm-"):
-            raise ValueError("Invalid message_id prefix. Must start with llm-")
-    
-    def _assert_proper_rating(self) -> None:
+    @field_validator("value")
+    def assert_proper_rating(cls, v: str) -> str:
         """
         Validate the rating value.
         """
 
-        if self.value not in ["Good", "Bad"]:
+        if v not in ["Good", "Bad"]:
             raise ValueError("Invalid rating given. Must be either 'Good' or 'Bad'.")
+        
+        return v
---
+++ src/main/app/backend/tests/test_rating.py
@@ -0,0 +1,20 @@
+import unittest
+
+from objects.rating import Rating
+
+class TestRating(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        pass
+
+    def test_init(self) -> None:
+        r = Rating(session_id="s-123", conversation_id="conv-123", message_id="llm-123", value="Good", message="good job.")
+        r2 = Rating(session_id="s-123", conversation_id="conv-123", message_id="llm-123", value="Bad")
+
+    def test_bad_init(self) -> None:
+        with self.assertRaises(ValueError):
+            Rating(session_id="conv-123", conversation_id="conv-123", message_id="llm-123", value="Bad")
+            Rating(session_id="s-123", conversation_id="s-123", message_id="llm-123", value="Bad")
+            Rating(session_id="s-123", conversation_id="conv-123", message_id="user-123", value="Bad")
+
